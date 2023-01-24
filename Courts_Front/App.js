f1901691d11a930cc2e9d48af5230f7dacbcd5e0
Peer <peerfichman@gmail.com>
2023-01-24 14:06:24 +0200

    first commit to navbar

--- Courts_Front/App.js
+++ Courts_Front/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello Pe'er</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+//
+import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
+import Example from "./Components/Navbar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Pe'er</Text>
-      <StatusBar style="auto" />
-    </View>
+          <Example/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
