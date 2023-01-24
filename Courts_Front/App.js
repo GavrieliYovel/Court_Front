08c7bb407d00bd410aa43a659186a23098d59804
Peer <peerfichman@gmail.com>
2023-01-24 18:37:33 +0200

    vcommit

--- Courts_Front/App.js
+++ Courts_Front/App.js
@@ -4,24 +4,24 @@ import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Hello Pe'er</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-      <Navbar/>
+    <View style={styles.container}>
+      <Text>Hello Pe'er</Text>
+      <StatusBar style="auto" />
+    </View>
+     // <Navbar/>
 
 
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 // import React from "react";
 // import {NativeBaseProvider, Box} from "native-base";
--- Courts_Front/Components/Navbar.js
+++ Courts_Front/Components/Navbar.js
@@ -1,37 +1,18 @@
-import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import TestScreen1 from '../Screens/testScreen1'
-import testScreen2 from '../Screens/testScreen2'
-
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
-
-import DrawerItems from "./DrawerItems";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+import TestScreen1 from "../Screens/testScreen1";
+import TestScreen2 from "../Screens/testScreen2";
 
 
-const Drawer = createDrawerNavigator();
+const RootDrawerNavigator = createDrawerNavigator({
+    Test1:{
+        screen: TestScreen1,
+    },
+    Test2:{
+        screen: TestScreen2,
+    }
 
-export default function Navbar() {
-    return (
-        <NavigationContainer>
-            <Drawer.Navigator
-                drawerType="front"
-                initialRouteName="Profile"
-                useLegacyImplementation
-                screenOptionsw={{
-                    activeTintColor: '#e91e63',
-                    itemStyle: { marginVertical: 10 },
-                }}
+});
 
-            >
-                {
-                    DrawerItems.map(drawer=> <Drawer.Screen
-                        key={drawer.name} name={drawer.name} component={TestScreen1}
-                    />)
-                }
-            </Drawer.Navigator>
-        </NavigationContainer>
-    );
-}
+export default createAppContainer(RootDrawerNavigator);
+
