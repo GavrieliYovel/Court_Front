d0524b61194069b632c36bee78bb1e0182a75a97
GavrieliYovel <yovel.gavrieli@stu.shenkar.ac.il>
2023-01-24 18:53:39 +0200

    maps

--- Courts_Front/App.js
+++ Courts_Front/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState, useEffect} from "react";
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Map } from './views/map'
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Pe'er</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <Map />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    );
+}
